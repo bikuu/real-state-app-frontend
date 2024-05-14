34c38fb26f5f064b109259387d1fbd2e5e899813
bikuu <bikuu@github.com>
2024-05-14 19:22:42 +0545

    finish profile update component and image upload feature

--- src/pages/updateProfile/UpdateProfile.jsx
+++ src/pages/updateProfile/UpdateProfile.jsx
@@ -1,15 +1,38 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./updateProfile.scss";
 import { AuthContext } from "../../context/AuthContext";
 import { FaUser } from "react-icons/fa";
+import { apiRequest } from "./../../lib/apiRequest";
+import { useNavigate } from "react-router-dom";
+import UploadWidget from "./../../components/uploadWidget/UploadWidget";
 
 function ProfileUpdatePage() {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
   const { currentUser, updateUser } = useContext(AuthContext);
-
+  const [avatar, setAvatar] = useState(currentUser.avatar);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
+    try {
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+        avatar,
+      });
+      updateUser(res.data);
+      navigate("/profile");
+    } catch (error) {
+      console.log(error);
+      setError(error.resposne.data.message);
+    }
+  };
   return (
     <div className="profileUpdatePage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
@@ -34,14 +57,25 @@ function ProfileUpdatePage() {
             <input id="password" name="password" type="password" />
           </div>
           <button>Update</button>
+          {error && <span>{error}</span>}
         </form>
       </div>
       <div className="sideContainer">
-        {currentUser.avatar ? (
-          <img src={currentUser.avatar} alt="" className="imgIcon" />
+        {avatar ? (
+          <img src={avatar} alt="" className="imgIcon" />
         ) : (
           <FaUser className="imgIcon" />
         )}
+        <UploadWidget
+          uwConfig={{
+            cloudName: "dlnht8dzh",
+            uploadPreset: "real-state",
+            multiple: false,
+            maxImageFileSize: 2000000,
+            folder: "avatars",
+          }}
+          setAvatar={setAvatar}
+        />
       </div>
     </div>
   );
